e1156e96b48341e22f607e331e1249593c6605ed
Stan <st.takt@gmail.com>
2024-01-18 17:44:01 +0300

    conecting to  API WIP

--- new-frontend/next.config.js
+++ new-frontend/next.config.js
@@ -5,6 +5,17 @@ const nextConfig = {
   images: {
     domains: ["cdn.jsdelivr.net"],
   },
+  rewrites: async () => {
+    return [
+      {
+        source: "/api/:path*",
+        destination:
+          process.env.NODE_ENV === "development"
+            ? "http://127.0.0.1:8000/api/:path*"
+            : "/api/",
+      },
+    ];
+  }
 };
 
 module.exports = nextConfig;
